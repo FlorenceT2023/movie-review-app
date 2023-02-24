ea0749fd3c9348b6a317e199a4ec6d32723e9c07
Florence Tat <florence.tat28@ed.cna.nl.ca>
2023-02-24 11:04:33 -0330

    modified Form.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -17,7 +17,6 @@ export function LeaveReview(props) {
     //         movieData.push(movie);
     //     })
 
-    //     const id = props.movies.length + 1;
     //     const title = movieTitle.current.value;
     //     const releaseDate = movieReleaseDate.current.value;
     //     const actors = movieActors.current.value;
@@ -26,7 +25,7 @@ export function LeaveReview(props) {
         
     //     movieData.push({"id": id, "title": title, "release_date": releaseDate, "actors": actors, "image": poster, "rating": rating});
     //     props.setMovies(movieData);
-
+    //     const id = props.movies.length + 1;
     //     alert(`${title} has been added!`);
     //     movieTitle.current.value = "";
     //     movieReleaseDate.current.value = "";
@@ -35,29 +34,32 @@ export function LeaveReview(props) {
     //     movieRating.current.value = "";
     // }
 
-    
+
     return (
         <>
         <center>
             <h2>Submit Review</h2>
-            <form method="post" action="/updateMovies">
+            <form method="post">
                 <label> Movie Title: 
                     <input
+                    name = "title"
                     ref = {movieTitle}
                     type = "text" />
                 </label><br />
                 <label> Release Date (yyyy-mm-dd):
                     <input
+                    name = "release_date"
                     ref = {movieReleaseDate}
                     type = "text" />
                 </label><br />
                 <label> Actors:
                     <input
+                    name="actors"
                     ref = {movieActors}
                     type = "text" />
                 </label><br />
                 <label> Movie Poster:
-                    <select ref={moviePoster}>
+                    <select name = "image" ref={moviePoster}>
                         <option>/images/popcorn_placeholder.png</option>
                         <option>/images/terminator2.jpg</option>
                         <option>/images/grand_budapest_hotel.jpg</option>
@@ -67,7 +69,7 @@ export function LeaveReview(props) {
                     </select>
                 </label><br />
                 <label> Rating:
-                    <select ref={movieRating}>
+                    <select name="rating" ref={movieRating}>
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
