cddf37f5e27cc3df46dad02f28e08c763d4372a3
Florence Tat <florence.tat28@ed.cna.nl.ca>
2023-02-23 21:22:50 -0330

    updating form.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -9,54 +9,57 @@ export function LeaveReview(props) {
     const moviePoster = useRef();
     const movieRating = useRef();
     
-    const submit = (event) => {
+    // const submit = (event) => {
 
-        event.preventDefault();
-        const movieData = [];
-        props.movies.forEach( movie => {
-            movieData.push(movie);
-        })
+    //     event.preventDefault();
+    //     const movieData = [];
+    //     props.movies.forEach( movie => {
+    //         movieData.push(movie);
+    //     })
 
-        const id = props.movies.length + 1;
-        const title = movieTitle.current.value;
-        const releaseDate = movieReleaseDate.current.value;
-        const actors = movieActors.current.value;
-        const poster = moviePoster.current.value;
-        const rating = movieRating.current.value;
+        // const id = props.movies.length + 1;
+    //     const title = movieTitle.current.value;
+    //     const releaseDate = movieReleaseDate.current.value;
+    //     const actors = movieActors.current.value;
+    //     const poster = moviePoster.current.value;
+    //     const rating = movieRating.current.value;
         
-        movieData.push({"id": id, "title": title, "release_date": releaseDate, "actors": actors, "image": poster, "rating": rating});
-        props.setMovies(movieData);
+    //     movieData.push({"id": id, "title": title, "release_date": releaseDate, "actors": actors, "image": poster, "rating": rating});
+    //     props.setMovies(movieData);
 
-        alert(`${title} has been added!`);
-        movieTitle.current.value = "";
-        movieReleaseDate.current.value = "";
-        movieActors.current.value = "";
-        moviePoster.current.value = "";
-        movieRating.current.value = "";
-    }
+    //     alert(`${title} has been added!`);
+    //     movieTitle.current.value = "";
+    //     movieReleaseDate.current.value = "";
+    //     movieActors.current.value = "";
+    //     moviePoster.current.value = "";
+    //     movieRating.current.value = "";
+    // }
 
     return (
         <>
         <center>
             <h2>Submit Review</h2>
-            <form onSubmit={submit}>
+            <form method="post" >
                 <label> Movie Title: 
                     <input
+                    name="title"
                     ref = {movieTitle}
                     type = "text" />
                 </label><br />
                 <label> Release Date (yyyy-mm-dd):
                     <input
+                    name="release_date"
                     ref = {movieReleaseDate}
                     type = "text" />
                 </label><br />
                 <label> Actors:
                     <input
+                    name="actors"
                     ref = {movieActors}
                     type = "text" />
                 </label><br />
                 <label> Movie Poster:
-                    <select ref={moviePoster}>
+                    <select name="image" ref={moviePoster}>
                         <option>/images/popcorn_placeholder.png</option>
                         <option>/images/terminator2.jpg</option>
                         <option>/images/grand_budapest_hotel.jpg</option>
@@ -66,7 +69,7 @@ export function LeaveReview(props) {
                     </select>
                 </label><br />
                 <label> Rating:
-                    <select ref={movieRating}>
+                    <select name="rating" ref={movieRating}>
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -74,7 +77,7 @@ export function LeaveReview(props) {
                         <option>5</option>
                     </select>
                 </label><br />
-                <button>ADD</button>
+                <input type="submit" value="ADD" />
             </form>            
         </center>
         </>
